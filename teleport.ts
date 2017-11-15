00b04b5b74e79b8dccb81a30e3bed87a2a200926
scottaubrey <scott@aubrey.org.uk>
2017-11-15 17:06:07 +0000

    Update teleport.ts

--- teleport.ts
+++ teleport.ts
@@ -1,12 +1,8 @@
 const magik = magikcraft.io;
 
 function teleport(name) {
-const there = magik.memento.getItem(name);
-if (there)	{
-    magik.ianuae(there)
-} else {
-    const here = magik.hic();
-    magik.memento(here);
-    
-    }
+    const there = magik.memento.getItem(name);
+    if (there)	{
+        magik.ianuae(there)
+    } 
 }
